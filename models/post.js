3f888cfad3a7bb13f2eeb686962a0f4714364c26
Robert Reinert <robert.reinert@gmail.com>
2021-02-09 13:46:52 -0800

    first commit

---
+++ models/post.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
+var Schema = mongoose.Schema;
+
+var PostSchema = new Schema({
+  title: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: "User"},
+  timestamp: { type: Date },
+  content: { type: String },
+});
+
+PostSchema.virtual("timestamp_formatted").get(function () {
+  return this.timestamp ? DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATETIME_MED) : ""
+});
+
+module.exports = mongoose.model("Post", PostSchema);
